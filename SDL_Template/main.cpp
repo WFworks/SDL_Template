f01ee391d74f5885ae8e0134004fc7d48940200d
Daisuke Nishimori <daisukn@outlook.com>
2025-11-23 23:42:52 +0900

    Initial Commit

---
+++ SDL_Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_Template
+//
+//  Created by Daisuke Nishimori on 2025/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
